5b3994d61cb5d9655aae970c08f891cee0051a24
iaaanz <iangrasman@gmail.com>
2021-08-01 18:18:43 -0300

    fix: kick tec id

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'kicktec',
   description: '',
   execute(message, args) {
-    const member = message.guild.member('COLOCAR O ID DO TEC');
+    const member = message.guild.member('286648520252063744');
     // member.kick(); kicka do servidor, não usar
     member.voice.kick();
   },
